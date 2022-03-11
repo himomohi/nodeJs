627b966f1c8335cf9318f67977a13234c76d2d88
appcaster <himomohi@gmail.com>
2022-03-11 09:18:03 +0900

    first

---
+++ server.js
@@ -0,0 +1,22 @@
+// node.js 에서 쓰는 import 같은 개념
+var mysql = require('mysql');
+
+//connection 변수에 mysql 안에 있는 .createConnerction 함수 호출 ({객체})
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345',
+    database: 'ppic'
+});
+
+//커넥션 연결
+connection.connect();
+
+//쿼리 전송
+connection.query('SELECT no from board', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+
+});
+//커넥션 닫기
+connection.end(); 
